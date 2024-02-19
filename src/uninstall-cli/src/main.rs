5796ac37c64694a0291cc9df2bb8bf029e655a9d
lighttigerxiv <lighttigerxiv@gmail.com>
2024-02-19 22:44:57 +0000

    update uninstall script

--- src/uninstall-cli/src/main.rs
+++ src/uninstall-cli/src/main.rs
@@ -1,3 +1,5 @@
+use whiskers_launcher_rs::paths::{get_autostart_path, get_local_dir};
+
 //Imports only used in windows
 #[cfg(target_os = "windows")]
 use {
@@ -7,10 +9,7 @@ use {
 
 //Imports only used in linux
 #[cfg(target_os = "linux")]
-use {
-    simple_kl_rs::paths::{get_autostart_path, get_local_dir},
-    std::{fs, process::Command},
-};
+use std::{fs, process::Command};
 
 #[cfg(target_os = "windows")]
 fn press_to_close() {
@@ -25,31 +24,30 @@ fn main() {
     if cfg!(target_os = "linux") {
         let local_dir = get_local_dir().unwrap();
 
-        let remove_root_files_cmd = format!("sudo rm -f /usr/bin/simple-keyboard-launcher /usr/bin/simple-kl-service /usr/share/pixmaps/simple-kl.png /usr/share/applications/simple-kl.desktop");
+        let remove_binary_files_command = format!("sudo rm -f /usr/bin/whiskers-launcher /usr/bin/whiskers-launcher-companion /usr/share/pixmaps/whiskers-launcher.png /usr/share/applications/whiskers-launcher.desktop");
 
-        let remove_root_files_result = Command::new("sh")
+        let remove_binary_files_result = Command::new("sh")
             .arg("-c")
-            .arg(remove_root_files_cmd)
+            .arg(remove_binary_files_command)
             .output()
             .expect("Error removing file");
 
         println!("Removing files ...");
 
-        if !remove_root_files_result.status.success() {
+        if !remove_binary_files_result.status.success() {
             println!(
                 "Error removing files: {}",
-                String::from_utf8(remove_root_files_result.stderr).unwrap()
+                String::from_utf8(remove_binary_files_result.stderr).unwrap()
             );
         }
 
         let mut auto_start_file = get_autostart_path().unwrap();
-        auto_start_file.push("simple-kl-service.desktop");
+        auto_start_file.push("whiskers-launcher.desktop");
 
         if auto_start_file.exists() {
             fs::remove_file(&auto_start_file).expect("Error deleting autostart file");
         }
 
-        //Removing files
         if local_dir.exists() {
             fs::remove_dir_all(&local_dir).expect("Error removing local folder");
         }
--- src/uninstall-cli/src/windows-uninstall.ps1
+++ src/uninstall-cli/src/windows-uninstall.ps1
@@ -1,6 +1,6 @@
-$localDir = $env:APPDATA + "\simple-kl"
-$shortcut = $env:ALLUSERSPROFILE + "\Microsoft\Windows\Start Menu\Programs\Simple Keyboard Launcher.lnk"
-$autoStartShortcut = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs\Startup\Simple-KL-Service.lnk"
+$localDir = $env:APPDATA + "\whiskers-launcher"
+$shortcut = $env:ALLUSERSPROFILE + "\Microsoft\Windows\Start Menu\Programs\Whiskers Launcher.lnk"
+$autoStartShortcut = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs\Startup\Whiskers-Launcher-Companion.lnk"
 
 if(Test-Path -Path $localDir){Remove-Item $localDir}
 if(Test-Path -Path $shortcut){Remove-Item $shortcut}
